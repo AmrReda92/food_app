689c54c25f402283559abbdc5c77f61f8eabcf84
Amr Reda <amr.reda2014@yahoo.com>
2025-12-12 11:52:41 +0200

    added Bottom nav bar

--- lib/food_app.dart
+++ lib/food_app.dart
@@ -1,3 +1,4 @@
+import 'package:delivery_food_app/feature/bottom_navigation_bar/ui/bottom_nav_bar_screen.dart';
 import 'package:delivery_food_app/feature/home/ui/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -18,7 +19,7 @@ class FoodApp extends StatelessWidget {
           fontFamily: "Poppins"
         ),
        debugShowCheckedModeBanner: false,
-       home: HomeScreen(),
+       home: BottomNavBarScreen(),
       ),
     );
   }
